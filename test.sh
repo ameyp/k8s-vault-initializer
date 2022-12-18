5f8e8cb5ad87212acefff5211694b8a495801cfa
Amey Parulekar <amey@wirywolf.com>
2022-12-17 20:08:02 -0800

    Fix condition syntax in test.

--- test.sh
+++ test.sh
@@ -7,7 +7,9 @@ helm install vault hashicorp/vault -f test/vault-values.yaml
 kubectl apply -f test/rbac.yaml
 kubectl apply -f test/pod.yaml
 
-while $(kubectl get pod vault-0 -o jsonpath="{.status.containerStatuses[0].ready}") != 'true'; do
+while [[ $(kubectl get pod vault-0 -o jsonpath="{.status.containerStatuses[0].ready}") != 'true' ]]; do
     echo "Vault is not yet unsealed"
     sleep 10
 done
+
+echo "Vault unsealed"
