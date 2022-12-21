3fc3ca543d2edffbd448ff333ef5b81c23f339b0
Amey Parulekar <amey@wirywolf.com>
2022-12-21 09:23:27 -0800

    Set up vault cluster with kubernetes auth and transit secrets.

--- main.go
+++ main.go
@@ -34,6 +34,37 @@ type VaultUnsealSubmission struct {
 	Key string
 }
 
+type VaultSecretEngine struct {
+	Type string `json:"type"`
+}
+
+type VaultKubernetesConfig struct {
+	KubernetesHost string `json:"kubernetes_host"`
+}
+
+type VaultPolicy struct {
+	Policy string `json:"policy"`
+}
+
+type VaultRole struct {
+	BoundServiceAccountNames []string `json:"bound_service_account_names"`
+	BoundServiceAccountNamespaces []string `json:"bound_service_account_namespaces"`
+	TokenPeriod string `json:"token_period"`
+	TokenPolicies []string `json:"token_policies"`
+}
+
+func (sealConfig *VaultSealConfig) toMap() map[string]string {
+	sealMap := make(map[string]string)
+
+	sealMap["rookToken"] = sealConfig.RootToken
+
+	for i := 0; i < len(sealConfig.Keys); i++ {
+		sealMap[fmt.Sprintf("key%d", i+1)] = sealConfig.Keys[i]
+	}
+
+	return sealMap
+}
+
 func requireEnv(variable string) []byte {
 	value := os.Getenv(variable)
 	if value == "" {
@@ -56,16 +87,16 @@ func isVaultInitialized(vault_addr string) (bool, error) {
 	endpoint := fmt.Sprintf("%s/v1/sys/init", vault_addr)
 	resp, err := http.Get(endpoint)
 	if err != nil {
-		return false, err
+		return false, fmt.Errorf("Could not create GET request: %w", err)
 	}
 	defer resp.Body.Close()
 
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return false, err
+		return false, fmt.Errorf("Could not read response of init: %w", err)
 	}
 
-	if resp.StatusCode != 200 {
+	if resp.StatusCode != 200 && resp.StatusCode != 204 {
 		return false, errors.New(
 			fmt.Sprintf("GET response status code: %v, body: %s", resp.StatusCode, string(body)))
 	}
@@ -81,20 +112,20 @@ func initializeVault(vault_addr string) (*VaultSealConfig, error) {
 	data, err := json.Marshal(VaultInitConfig{SecretShares: 5, SecretThreshold: 3})
 
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("Could not marshal VaultInitConfig: %w", err)
 	}
 
 	resp, err := http.Post(endpoint, "application/json", bytes.NewBuffer(data))
 
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("Could not make POST request: %w", err)
 	}
 
 	defer resp.Body.Close()
-	if resp.StatusCode != 200 {
+	if resp.StatusCode != 200 && resp.StatusCode != 204 {
 		responseBody, err := ioutil.ReadAll(resp.Body)
 		if err != nil {
-			return nil, err
+			return nil, fmt.Errorf("Could not read response of vault initialization: %w", err)
 		}
 		return nil, errors.New(fmt.Sprintf(
 			"POST response status code: %v, body: %s", resp.StatusCode, string(responseBody)))
@@ -109,31 +140,124 @@ func initializeVault(vault_addr string) (*VaultSealConfig, error) {
 	return &sealConfig, nil
 }
 
-func submitUnsealKey(vault_addr string, key string) error {
-	endpoint := fmt.Sprintf("%s/v1/sys/unseal", vault_addr)
-	data, err := json.Marshal(VaultUnsealSubmission{Key: key})
-
-	if err != nil {
-		return err
+func makePostRequest(endpoint string, data []byte, token ...string) error {
+	client := &http.Client{}
+	var dataReader io.Reader
+	if data != nil {
+		dataReader = bytes.NewReader(data)
 	}
 
-	resp, err := http.Post(endpoint, "application/json", bytes.NewBuffer(data))
-
+	req, err := http.NewRequest("POST", endpoint, dataReader)
 	if err != nil {
-		return err
+		return fmt.Errorf("Could not create POST request: %w", err)
+	}
+
+	req.Header.Add("Content-Type", "application/json")
+	if len(token) != 0 {
+		req.Header.Add("X-Vault-Token", token[0])
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return fmt.Errorf("Could not make POST request: %w", err)
 	}
 
 	defer resp.Body.Close()
-	if resp.StatusCode != 200 {
+	if resp.StatusCode != 200 && resp.StatusCode != 204 {
 		responseBody, err := ioutil.ReadAll(resp.Body)
 		if err != nil {
-			return err
+			return fmt.Errorf("Could not read POST response: %w", err)
 		}
 		return errors.New(fmt.Sprintf(
 			"POST response status code: %v, body: %s", resp.StatusCode, string(responseBody)))
 	}
 
 	return nil
+}
+
+func submitUnsealKey(vault_addr string, key string) error {
+	data, err := json.Marshal(VaultUnsealSubmission{Key: key})
+
+	if err != nil {
+		return fmt.Errorf("Could not marshal VaultUnsealSubmission: %w", err)
+	}
+
+	endpoint := fmt.Sprintf("%s/v1/sys/unseal", vault_addr)
+	return makePostRequest(endpoint, data)
+}
+
+func enableTransitEngine(vault_addr string, token string) error {
+	data, err := json.Marshal(VaultSecretEngine{Type: "transit"})
+	if err != nil {
+		return fmt.Errorf("Could not marshal VaultSecretEngine: %w", err)
+	}
+
+	endpoint := fmt.Sprintf("%s/v1/sys/mounts/transit", vault_addr)
+	return makePostRequest(endpoint, data, token)
+}
+
+// curl $VAULT_ADDR/v1/transit/keys/autounseal -H "X-Vault-Token: $VAULT_TOKEN" -X POST
+func createAutoUnsealKey(vault_addr string, token string) error {
+	endpoint := fmt.Sprintf("%s/v1/transit/keys/autounseal", vault_addr)
+	return makePostRequest(endpoint, nil, token)
+}
+
+// curl $VAULT_ADDR/v1/sys/auth/kubernetes -H "X-Vault-Token: $VAULT_TOKEN" -X POST --data-raw '{"type": "kubernetes"}'
+func enableKubernetesAuth(vault_addr string, token string) error {
+	data, err := json.Marshal(VaultSecretEngine{Type: "kubernetes"})
+	if err != nil {
+		return fmt.Errorf("Could not marshal VaultSecretEngine: %w", err)
+	}
+
+	endpoint := fmt.Sprintf("%s/v1/sys/auth/kubernetes", vault_addr)
+	return makePostRequest(endpoint, data, token)
+}
+
+// curl $VAULT_ADDR/v1/auth/kubernetes/config -H "X-Vault-Token: $VAULT_TOKEN" -X POST --data-raw '{"kubernetes_host": "https://kubernetes.default.svc"}'
+func configureKubernetesAuth(vault_addr, token string) error {
+	data, err := json.Marshal(VaultKubernetesConfig{KubernetesHost: "https://kubernetes.default.svc"})
+	if err != nil {
+		return fmt.Errorf("Could not marshal VaultKubernetesConfig: %w", err)
+	}
+
+	endpoint := fmt.Sprintf("%s/v1/auth/kubernetes/config", vault_addr)
+	return makePostRequest(endpoint, data, token)
+}
+
+// curl $VAULT_ADDR/v1/sys/policy/autounseal -H "X-Vault-Token: $VAULT_TOKEN" -X POST --data-raw '{"policy": "path \"transit/encrypt/autounseal\" { capabilities = [ \"update\" ] } \n\n path \"transit/decrypt/autounseal\" { capabilities = [ \"update\" ] }"}'
+func createAutoUnsealPolicy(vault_addr, token string) error {
+	policy := `
+path "transit/encrypt/autounseal" {
+  capabilities = [ "update" ]
+}
+
+path "transit/decrypt/autounseal" {
+  capabilities = [ "update" ]
+}
+`
+	data, err := json.Marshal(VaultPolicy{Policy: policy})
+	if err != nil {
+		return fmt.Errorf("Could not marshal VaultPolicy: %w", err)
+	}
+
+	endpoint := fmt.Sprintf("%s/v1/sys/policy/autounseal", vault_addr)
+	return makePostRequest(endpoint, data, token)
+}
+
+// curl $VAULT_ADDR/v1/auth/kubernetes/role/autounseal -H "X-Vault-Token: $VAULT_TOKEN" -X POST --data-raw '{"bound_service_account_names": ["vault"], "bound_service_account_namespaces": ["vault", "default"], "token_period": "3600", "token_policies": ["autounseal"]}'
+func createAutoUnsealRole(vault_addr, token string) error {
+	data, err := json.Marshal(VaultRole{
+		BoundServiceAccountNames: []string{"vault"},
+		BoundServiceAccountNamespaces: []string{"vault", "default"},
+		TokenPeriod: "3600",
+		TokenPolicies: []string{"autounseal"},
+	})
+	if err != nil {
+		return fmt.Errorf("Could not marshal VaultRole: %w", err)
+	}
+
+	endpoint := fmt.Sprintf("%s/v1/auth/kubernetes/role/autounseal", vault_addr)
+	return makePostRequest(endpoint, data, token)
 }
 
 func main() {
@@ -165,14 +289,9 @@ func main() {
 	}
 
 	log.Println("Initialized vault.")
-	sealJson, err := json.Marshal(sealConfig)
-	if err != nil {
-		log.Fatalf("Could not convert seal config to byte array")
-	}
 
+	secretData := sealConfig.toMap()
 	log.Println("Serialized and encoded vault seal configuration, creating secret.")
-
-	secretData := map[string]string{"seal-config": string(sealJson)}
 
 	namespace := getNamespace()
 	secretsManager, err := k8s_secrets.GetSecretsManager(namespace)
@@ -191,4 +310,36 @@ func main() {
 	for i := 0; i < 3; i += 1 {
 		submitUnsealKey(vault_addr, sealConfig.Keys[i])
 	}
+
+	log.Println("Enabling transit engine.")
+	if err = enableTransitEngine(vault_addr, sealConfig.RootToken); err != nil {
+		log.Fatalf("Could not enable transit engine: %s", err.Error())
+	}
+
+	log.Println("Creating autounseal key.")
+	if err = createAutoUnsealKey(vault_addr, sealConfig.RootToken); err != nil {
+		log.Fatalf("Could not create autounseal key: %s", err.Error())
+	}
+
+	log.Println("Enabling kubernetes auth.")
+	if err = enableKubernetesAuth(vault_addr, sealConfig.RootToken); err != nil {
+		log.Fatalf("Could not enable kubernetes auth: %s", err.Error())
+	}
+
+	log.Println("Configuring kubernetes auth")
+	if err = configureKubernetesAuth(vault_addr, sealConfig.RootToken); err != nil {
+		log.Fatalf("Could not configure kubernetes auth: %s", err.Error())
+	}
+
+	log.Println("Creating autounseal policy")
+	if err = createAutoUnsealPolicy(vault_addr, sealConfig.RootToken); err != nil {
+		log.Fatalf("Could not create autounseal policy: %s", err.Error())
+	}
+
+	log.Println("Creating autounseal role")
+	if err = createAutoUnsealRole(vault_addr, sealConfig.RootToken); err != nil {
+		log.Fatalf("Could not create autounseal role: %s", err.Error())
+	}
+
+	// curl http://vault.default.svc:8200/v1/auth/kubernetes/login -X POST --data-raw "{\"role\": \"redis\", \"jwt\": \"$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)\"}"
 }
--- test.sh
+++ test.sh
@@ -1,15 +1,46 @@
 #!/bin/bash
 
+# Install vault
 helm repo add hashicorp https://helm.releases.hashicorp.com
 helm repo update
-helm install vault hashicorp/vault -f test/vault-values.yaml
+helm install vault-unsealer hashicorp/vault -f test/vault-unsealer-values.yaml
 
+# Create service accounts, roles and bindings
 kubectl apply -f test/rbac.yaml
+
+# Create a pod with the image we want to test
 kubectl apply -f test/pod.yaml
 
-while [[ $(kubectl get pod vault-0 -o jsonpath="{.status.containerStatuses[0].ready}") != 'true' ]]; do
+# Wait for vault to be initialized and unsealed by the pod
+while [[ $(kubectl get pod vault-unsealer-0 -o jsonpath="{.status.containerStatuses[0].ready}") != 'true' ]]; do
     echo "Vault is not yet unsealed"
     sleep 10
 done
 
-echo "Vault unsealed"
+echo "Vault unsealed, transit engine and kubernetes auth enabled."
+
+# Port forward vault's HTTP port
+LOCAL_PORT="45000"
+kubectl port-forward pods/vault-unsealer-0 $LOCAL_PORT:8200 &
+
+# Get a valid service account token for an account authorized to access the autounseal transit engine
+SERVICE_ACCOUNT_TOKEN=$(kubectl create token vault)
+
+# Login using the service account token and get a valid vault token
+VAULT_TOKEN=$(curl -s http://localhost:$LOCAL_PORT/v1/auth/kubernetes/login -X POST --data-raw "{\"role\": \"autounseal\", \"jwt\": \"$SERVICE_ACCOUNT_TOKEN\"}" | jq -r '.auth.client_token')
+
+# We just care that the request succeeds and returns encrypted text.
+curl -s http://localhost:$LOCAL_PORT/v1/transit/encrypt/autounseal -H "X-Vault-Token: $VAULT_TOKEN" -X POST --data-raw '{"plaintext": "abcd"}' | jq -r '.data.ciphertext'
+
+# For the second vault cluster
+
+# Create the certificate
+openssl req -x509 -newkey rsa:4096 -sha256 -days 1 -nodes \
+  -keyout vault.key -out vault.crt -subj "/CN=*.vault-internal" \
+  -addext "subjectAltName=DNS:*.vault-internal,IP:127.0.0.1"
+
+# Create the secret
+kubectl create secret tls vault-internal-tls-secret --key="vault.key" --cert="vault.crt"
+
+# Remove local cert files
+rm -f vault.crt vault.key
