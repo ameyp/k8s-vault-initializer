f84e21ae57f2779ff314b821bd272176fb8d2212
Amey Parulekar <amey@wirywolf.com>
2022-12-22 08:35:40 -0800

    Make the test better.

--- test.sh
+++ test.sh
@@ -2,10 +2,37 @@
 
 set -euo pipefail
 
-echo "Install vault"
-helm repo add hashicorp https://helm.releases.hashicorp.com
-helm repo update
-helm install vault-unsealer hashicorp/vault -f test/vault-unsealer-values.yaml
+UNSEAL_INITIALIZER="vault-unsealer-init"
+
+function wait_for_pod_ready() {
+    local POD_NAME=$1
+
+    while [[ $(kubectl get pod $POD_NAME -o jsonpath="{.status.containerStatuses[0].ready}") != 'true' ]]; do
+        echo "$POD_NAME is not yet ready"
+        sleep 10
+    done
+}
+
+function wait_for_pod_termination() {
+    local POD_NAME=$1
+
+    echo "Wait for the initializer to terminate"
+    while [[ $(kubectl get pod $POD_NAME -o jsonpath="{.status.containerStatuses[0].ready}") != 'false' ]]; do
+        echo "$POD_NAME is not yet done"
+        sleep 10
+    done
+
+    if [[ $(kubectl get pod $POD_NAME -o jsonpath="{.status.containerStatuses[0].state.terminated.exitCode}") == '0' ]]; then
+        echo "$POD_NAME succeeded"
+    else
+        echo "Initializer failed"
+        echo "---- Pod details ----"
+        kubectl describe pod $POD_NAME
+        echo "---- Pod logs ----"
+        kubectl logs $POD_NAME
+        exit 1
+    fi
+}
 
 echo "Create service accounts, roles and bindings"
 kubectl apply -f test/rbac.yaml
@@ -13,11 +40,18 @@ kubectl apply -f test/rbac.yaml
 echo "Create a pod with the image we want to test"
 kubectl apply -f test/unsealer-init-pod.yaml
 
-echo "Wait for vault to be initialized and unsealed by the pod"
-while [[ $(kubectl get pod vault-unsealer-0 -o jsonpath="{.status.containerStatuses[0].ready}") != 'true' ]]; do
-    echo "Vault is not yet unsealed"
-    sleep 10
-done
+echo "Wait for the initializer to start"
+wait_for_pod_ready $UNSEAL_INITIALIZER
+
+echo "Install vault"
+helm repo add hashicorp https://helm.releases.hashicorp.com
+helm repo update
+helm install vault-unsealer hashicorp/vault -f test/vault-unsealer-values.yaml
+
+wait_for_pod_termination $UNSEAL_INITIALIZER
+
+echo "Wait for vault-unsealer to be ready"
+wait_for_pod_ready vault-unsealer-0
 
 echo "Vault unsealed, transit engine and kubernetes auth enabled."
 
@@ -25,6 +59,12 @@ echo "Port forward vault's HTTP port"
 LOCAL_PORT="45000"
 kubectl port-forward pods/vault-unsealer-0 $LOCAL_PORT:8200 &
 PID=$!
+
+echo "Wait for the port to be up"
+while [[ $(curl -s http://localhost:$LOCAL_PORT/v1/sys/health | jq ".sealed") != 'false' ]]; do
+    echo "Vault is not yet reachable on the local port forward"
+    sleep 2
+done
 
 echo "Get a valid service account token for an account authorized to access the autounseal transit engine"
 SERVICE_ACCOUNT_TOKEN=$(kubectl create token vault)
@@ -34,14 +74,16 @@ VAULT_TOKEN=$(curl -s http://localhost:$LOCAL_PORT/v1/auth/kubernetes/login -X P
 
 # We just care that the request succeeds and returns encrypted text.
 echo "Encrypting text using the transit engine"
-curl -s http://localhost:$LOCAL_PORT/v1/transit/encrypt/autounseal -H "X-Vault-Token: $VAULT_TOKEN" -X POST --data-raw '{"plaintext": "abcd"}' | jq -r '.data.ciphertext'
+curl -s "http://localhost:$LOCAL_PORT/v1/transit/encrypt/autounseal" \
+     -H "X-Vault-Token: $VAULT_TOKEN" \
+     -X POST --data-raw '{"plaintext": "abcd"}' | jq -r '.data.ciphertext'
 
 # For the second vault cluster
 
 echo "Create the certificate"
 openssl req -x509 -newkey rsa:4096 -sha256 -days 1 -nodes \
-  -keyout vault.key -out vault.crt -subj "/CN=*.vault-internal" \
-  -addext "subjectAltName=DNS:*.vault-internal,IP:127.0.0.1"
+        -keyout vault.key -out vault.crt -subj "/CN=*.vault-internal" \
+        -addext "subjectAltName=DNS:*.vault-internal,IP:127.0.0.1"
 
 echo "Create the secret"
 kubectl create secret tls vault-internal-tls-secret --key="vault.key" --cert="vault.crt"
